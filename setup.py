79f16f262aa9731c317b62f6f6152eda712b24bb
Tyberius Prime <tyberius_prime@coonabibba.de>
2024-11-18 10:39:58 +0100

    introducing pyproject.toml

--- setup.py
+++ setup.py
@@ -1,52 +1,40 @@
 from setuptools import setup, Extension
-import numpy
 
-NAME = 'cellstates'
-VERSION = '0.1'
-DESCR = 'Module for finding gene expression states in scRNAseq data'
-REQUIRES = ['numpy', 'pandas', 'matplotlib']
-
-AUTHOR = 'Pascal Grobecker'
-EMAIL = 'pascal.grobecker@unibas.ch'
-
-PACKAGES = ['cellstates']
+PACKAGES = ["cellstates"]
 
 
 USE_CYTHON = True
 try:
+    import numpy
     from Cython.Build import cythonize
-except ModuleNotFoundError:
+except (ModuleNotFoundError, ImportError):
     USE_CYTHON = False
 
-ext = '.pyx' if USE_CYTHON else '.c'
-
-EXTENSIONS = [Extension("cellstates.cluster",
-                        ["cellstates/cluster" + ext],
-                        include_dirs=[numpy.get_include(), '.'],
-                        extra_compile_args=['-fopenmp'],
-                        extra_link_args=['-fopenmp'],
-                        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
-              Extension("cellstates.chelpers",
-                        ["cellstates/chelpers" + ext],
-                        include_dirs=[numpy.get_include(), '.'],
-                        extra_compile_args=['-fopenmp'],
-                        extra_link_args=['-fopenmp'],
-                        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-                        )
-              ]
-
 if USE_CYTHON:
-    EXTENSIONS = cythonize(EXTENSIONS,
-                           compiler_directives={'language_level': 3})
+    ext = ".pyx" if USE_CYTHON else ".c"
 
-if __name__ == '__main__':
-    setup(
-        name=NAME,
-        version=VERSION,
-        description=DESCR,
-        author=AUTHOR,
-        author_email=EMAIL,
-        install_requires=REQUIRES,
-        packages=PACKAGES,
-        ext_modules=EXTENSIONS
-    )
+    EXTENSIONS = [
+        Extension(
+            "cellstates.cluster",
+            ["src/cellstates/cluster" + ext],
+            include_dirs=[numpy.get_include(), "."],
+            extra_compile_args=["-fopenmp"],
+            extra_link_args=["-fopenmp"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        ),
+        Extension(
+            "cellstates.chelpers",
+            ["src/cellstates/chelpers" + ext],
+            include_dirs=[numpy.get_include(), "."],
+            extra_compile_args=["-fopenmp"],
+            extra_link_args=["-fopenmp"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        ),
+    ]
+
+    EXTENSIONS = cythonize(EXTENSIONS, compiler_directives={"language_level": 3})
+else:
+    EXTENSIONS = []
+
+if __name__ == "__main__":
+    setup(ext_modules=EXTENSIONS)
